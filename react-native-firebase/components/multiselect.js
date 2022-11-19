0a0acea169b740fda1289bc2e2108cc0558a2321
Hanna Koh <hjk5643@g.rit.edu>
2022-11-19 13:15:49 -0500

    adding new files

---
+++ react-native-firebase/components/multiselect.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import MultiSelect from 'react-native-multiple-select';
+
+const items = [{
+        id: "001", 
+        name: "American"
+    }, {
+        id: "002", 
+        name: "Chinese"
+    }, { 
+        id: "003",
+        name: "Korean"
+    }, {
+        id: "004",
+        name: "Japanese"
+    }]
+
+export class MultiSelectExample extends Component {
+
+        state = {
+          selectedItems : []
+        };
+      
+        
+        onSelectedItemsChange = selectedItems => {
+          this.setState({ selectedItems });
+        };
+      
+        render() {
+          const { selectedItems } = this.state;
+      
+          return (
+            <View style={{ flex: 1 }}>
+              <MultiSelect
+                hideTags
+                items={items}
+                uniqueKey="id"
+                ref={(component) => { this.multiSelect = component }}
+                onSelectedItemsChange={this.onSelectedItemsChange}
+                submitButtonText="Submit"
+              />
+              <View>
+                {this.multiSelect.getSelectedItemsExt(selectedItems)}
+              </View>
+            </View>
+          );
+        }
+    }
--- react-native-firebase/screens/form.js
+++ react-native-firebase/screens/form.js
@@ -1,14 +1,35 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { MultiSelectExample } from '../components/multiselect';
 import Slider from '@react-native-community/slider';
 
 export function FormScreen() {
+    const [value, setValue] = useState(0);
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 
-      <Text>Home Screen</Text>
+      <Text>Answer these brief questions so we can help you pick a place!</Text>
+      <Text> </Text>
+      <Text>Select what you're interested in:</Text>
+      <MultiSelectExample/>
+      <Text>How hungry are you?</Text>
+      <Slider style={{width: 200, height: 20}} 
+        step={1}
+        minimumValue={0}
+        maximumValue={100}
+        value={value}
+        onValueChange={slideValue => setValue(slideValue)}
+        minimumTrackTintColor="#1fb28a"
+        maximumTrackTintColor="#d3d3d3"
+        thumbTintColor="#b9e4c9"
+       />
+       <Text>
+          Slide value: {value}%
+        </Text>
+
     </View>
+    
   );
 }
