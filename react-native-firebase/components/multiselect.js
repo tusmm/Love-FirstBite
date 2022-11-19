aaecca69cc4ba586dd2aedc9e451187bf7104aa6
Hanna Koh <hjk5643@g.rit.edu>
2022-11-19 14:50:00 -0500

    got most of the components on the form page!

--- react-native-firebase/components/multiselect.js
+++ react-native-firebase/components/multiselect.js
@@ -2,57 +2,58 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import MultiSelect from 'react-native-multiple-select';
 const items = [{
-    id: "001", 
+    id: "001",
     name: "American"
-  }, {
-    id: "002", 
+}, {
+    id: "002",
     name: "Chinese"
-  }, { 
+}, {
     id: "003",
     name: "Korean"
-  }, {
+}, {
     id: "004",
     name: "Japanese"
-  }]
-  
+}]
+
 export class MultiSelectFoods extends Component {
 
-        state = {
-          selectedItems : []
-        };
-    
-        onSelectedItemsChange = (selectedItems) => {
-          this.setState({ selectedItems });
-          console.log('Selected Items: ', selectedItems);
-        };
-      
-        render() {
-          const { selectedItems } = this.state;
-      
-          return (
-            <View style={{ width: 200, height:200 }}>
-               <MultiSelect
-          hideTags
-          items={items}
-          uniqueKey="id"
-          ref={(component) => { this.multiSelect = component }}
-          onSelectedItemsChange={this.onSelectedItemsChange}
-          selectedItems={selectedItems}
-          selectText="Pick Items"
-          searchInputPlaceholderText="Search Items..."
-          onChangeInput={ (text)=> console.log(text)}
-          tagRemoveIconColor="#CCC"
-          tagBorderColor="#CCC"
-          tagTextColor="#CCC"
-          selectedItemTextColor="#CCC"
-          selectedItemIconColor="#CCC"
-          itemTextColor="#000"
-          displayKey="name"
-          searchInputStyle={{ color: '#CCC' }}
-          submitButtonColor="#CCC"
-          submitButtonText="Submit"
-        />
+    state = {
+        selectedItems: []
+    };
+
+    onSelectedItemsChange = (selectedItems) => {
+        this.setState({ selectedItems });
+        console.log('Selected Items: ', selectedItems);
+    };
+
+    render() {
+        const { selectedItems } = this.state;
+
+        return (
+            <View style={{ width: 200, height: 50, elevation: 1, zIndex:1}}>
+                <MultiSelect
+                    hideTags
+                    styleDropdownMenu = {{elevation: -1, zIndex:1}}
+                    items={items}
+                    uniqueKey="id"
+                    ref={(component) => { this.multiSelect = component }}
+                    onSelectedItemsChange={this.onSelectedItemsChange}
+                    selectedItems={selectedItems}
+                    selectText="Pick Items"
+                    searchInputPlaceholderText="Search Items..."
+                    onChangeInput={(text) => console.log(text)}
+                    tagRemoveIconColor="#CCC"
+                    tagBorderColor="#CCC"
+                    tagTextColor="#CCC"
+                    selectedItemTextColor="#CCC"
+                    selectedItemIconColor="#CCC"
+                    itemTextColor="#000"
+                    displayKey="name"
+                    searchInputStyle={{ color: '#CCC' }}
+                    submitButtonColor="#CCC"
+                    submitButtonText="Submit"
+                />
             </View>
-          );
-        }
+        );
     }
+}
