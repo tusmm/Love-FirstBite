9e3e57ec99981e9111a6f86a1ed75fffb1bee0eb
tusmm <r.ong40915@gmail.com>
2022-11-19 15:01:52 -0500

    successful button to firebase

--- react-native-firebase/App.js
+++ react-native-firebase/App.js
@@ -2,17 +2,29 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { FormScreen } from './screens/form';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/firebase/Add';
 
-const Stack = createNativeStackNavigator();
+//  const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>{
-      <Stack.Navigator>
-        <Stack.Screen name=" " component={FormScreen} />
-      </Stack.Navigator>
-    }</NavigationContainer>
+    // <NavigationContainer>{
+    //   <Stack.Navigator>
+    //     <Stack.Screen name=" " component={FormScreen} />
+    //   </Stack.Navigator>
+    // }</NavigationContainer>
+
+    <View style = {styles.container}>
+      <Home/>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+  },
+});
 
 export default App;
--- react-native-firebase/src/firebase/config.js
+++ react-native-firebase/src/firebase/config.js
@@ -1,4 +1,5 @@
 import firebase from "firebase/compat/app"
+import "firebase/compat/firestore"
 import '@firebase/auth';
 import '@firebase/firestore';
 
