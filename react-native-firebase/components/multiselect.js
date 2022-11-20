e40cf93973cf905503029cebc22c2b57c24902e5
Hanna Koh <hjk5643@g.rit.edu>
2022-11-19 22:07:32 -0500

    this is so tiger has everything

--- react-native-firebase/components/multiselect.js
+++ react-native-firebase/components/multiselect.js
@@ -24,7 +24,6 @@ export class MultiSelectFoods extends Component {
             item.push(number)
         });
         this.props.getValue(item)
-        // console.log(item)
     };
 
     render() {
