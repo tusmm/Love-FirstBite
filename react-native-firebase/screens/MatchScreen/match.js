069aad9467d9986b38a073ba6d8e9884d7fd01cb
lh5844 <lh5844rit@gmail.com>
2022-11-20 05:17:12 -0500

    finished a temporary match restuls screen

--- react-native-firebase/screens/MatchScreen/match.js
+++ react-native-firebase/screens/MatchScreen/match.js
@@ -1,14 +1,97 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import Card from '../../components/restaurantcard';
+import React, { Component } from 'react';
+import { View, StyleSheet, Animated, Text, Image} from 'react-native';
 import restaurants from '../../data/restaurants';
+import logo_img from '../../assets/logo.png'
+/* 
+import Card from '../../components/restaurantcard';
 
 // so theoretically, you should be able to pass in a restaruant and itll display... except idk how to do that
 export function Match(){
     return(
         <View>
-            <Card name={restaurants[0]}/>
+            <Card restaurant={restaurants[0]}/>
             <Text>It's a match!</Text>
         </View>
     )
+}*/
+class ImageLoader extends Component {
+  state = {
+    opacity: new Animated.Value(0),
+  }
+
+  onLoad = () => {
+    Animated.timing(this.state.opacity, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  }
+
+  render() {
+    return (
+      <Animated.Image
+        onLoad={this.onLoad}
+        {...this.props}
+        style={[
+          {
+            opacity: this.state.opacity,
+            transform: [
+              {
+                scale: this.state.opacity.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0.5, 1],
+                })
+              },
+            ],
+          },
+          this.props.style,
+        ]}
+      />
+    );
+  }
 }
+
+export function OneTrueMatchScreen(){
+    return(
+    <View style={styles.container}>
+        <Image
+            style={styles.logo}
+            source = {logo_img}
+            resizeMode="contain"
+            >
+        </Image>
+        <Text style = {styles.match_text}>It's a Match!</Text>
+        <ImageLoader
+        style={styles.image}
+        source={{ uri: restaurants[0].image}}
+        />
+    </View>
+    );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#b67c7cff'
+  },
+  image: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+  },
+  match_text: {
+    fontSize: 30,
+    height: 100,
+    color: '#fff6f6ff',
+    fontWeight: 'bold',
+  },
+  logo: {
+    marginTop:"-15%",
+    height: 100,
+    width: 150,
+    margin: 40,
+  },
+});
+
