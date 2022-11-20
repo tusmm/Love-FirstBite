122f073ed676f36a6ef7b00d5afd3ec7979a84f7
lh5844 <lh5844rit@gmail.com>
2022-11-19 23:14:14 -0500

    working swipe screen

--- react-native-firebase/screens/swipescreen.js
+++ react-native-firebase/screens/swipescreen.js
@@ -1,21 +1,54 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Dimensions} from 'react-native';
 import Card from '../components/restaurantcard';
 import restaurants from '../data/restaurants';
+
+import AnimatedStack from '../components/AnimatedSwipe';
+
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const styles = StyleSheet.create({
     card_container: {
         justifyContent: 'center', 
         alignItems: 'center',
         flex: 1,
-        width: '100%',
+        width: Dimensions.get('window').width,
     },
 });
 
+var leftSwipes = []
+var rightSwipes = []
+
 export function SwipeScreen(){
+
+    const onSwipeLeft = restaurant => {
+        console.log('swipe left: ', restaurant.name);
+        leftSwipes.push(restaurant.name)
+    };
+    
+    const onSwipeRight = restaurant => {
+        console.log('swipe right: ', restaurant.name);
+        rightSwipes.push(restaurant.name)
+    };
+
     return (
         <View style = {styles.card_container}>
-            <Card restaurant ={restaurants[0]}/>
+            <GestureHandlerRootView>
+                <AnimatedStack 
+                data={restaurants}
+                renderItem={({item}) => <Card restaurant={item} />}
+                onSwipeLeft={onSwipeLeft}
+                onSwipeRight={onSwipeRight}
+                />
+             </GestureHandlerRootView>
         </View>
     );
+};
+
+export function getLeftSwipes(){
+    return(leftSwipes);
+};
+
+export function getRightSwipes(){
+    return(rightSwipes);
 };
